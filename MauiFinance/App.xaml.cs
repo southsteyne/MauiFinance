d6fdfc1fe904830a9898a536b737c8ca21294c4f
opilane <opilane@HP-CZC0359NN7>
2024-12-18 13:21:55 +0200

    Add project files.

---
+++ MauiFinance/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiFinance
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiFinance/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiFinance
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
