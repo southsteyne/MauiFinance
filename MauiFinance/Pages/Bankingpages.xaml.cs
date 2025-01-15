35dcc5a97e4349bb0cfe9ade6502b7449a943a09
opilane <opilane@HP-CZC0359NN8>
2025-01-15 11:01:18 +0200

    11:00

--- MauiFinance/Pages/Bankingpages.xaml.cs
+++ MauiFinance/Pages/Bankingpages.xaml.cs
@@ -1,9 +1,37 @@
-namespace Pages;
-
-public partial class Bankingpages : ContentPage
+namespace Pages
 {
-	public Bankingpages()
-	{
-		InitializeComponent();
-	}
+    public partial class Bankingpages : ContentPage
+    {
+        public Bankingpages()
+        {
+            InitializeComponent();
+        }
+
+        // Arvutamise meetod
+        private void OnCalculateBalanceClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(IncomeEntry.Text, out double income) &&
+                double.TryParse(ExpenseEntry.Text, out double expense))
+            {
+                double balance = income - expense;
+                BalanceLabel.Text = $"Saldo: {balance:C}";
+            }
+            else
+            {
+                BalanceLabel.Text = "Palun sisesta kehtivad summad.";
+            }
+        }
+
+        // Järgmisele lehele liikumise meetod
+        private async void OnNavigateToFinalPageClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FinalPage());
+        }
+
+        // Eelmisele lehele liikumise meetod
+        private async void OnNavigateToMainPageClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
 }
