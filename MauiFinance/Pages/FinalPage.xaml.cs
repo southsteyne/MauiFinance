72d784757467c0b425050c5b2926aab6618b2045
Opilane TTHK <foodlaska@gmail.com>
2025-01-15 12:56:37 +0200

    FinalPage (GERLI KOOD MINA EI TEINUD)
    See on Gerli kood!!!!!!!!!!!!!

--- MauiFinance/Pages/FinalPage.xaml.cs
+++ MauiFinance/Pages/FinalPage.xaml.cs
@@ -1,9 +1,24 @@
+using Microsoft.Maui.Controls;
+
 namespace Pages;
 
 public partial class FinalPage : ContentPage
 {
-	public FinalPage()
-	{
-		InitializeComponent();
-	}
+    public FinalPage()
+    {
+        InitializeComponent();
+
+        // Seome nupu toimingu käsu, et viia tagasi MainPage-le
+        var backButton = this.FindByName<Button>("BackToHomeButton");
+        backButton.Clicked += OnBackToHomeButtonClicked;
+    }
+
+    // Funktsioon, mis viib tagasi MainPage-le
+    private async void OnBackToHomeButtonClicked(object sender, EventArgs e)
+    {
+        // Siin kasutame Navigation.PopAsync() et viia tagasi eelnevalt avatud lehele (MainPage)
+        await Navigation.PopAsync();  // Kui kasutatakse navigationi Stack
+        // Või, kui kasutad shelli navigeerimist
+        // await Shell.Current.GoToAsync("//MainPage");
+    }
 }
